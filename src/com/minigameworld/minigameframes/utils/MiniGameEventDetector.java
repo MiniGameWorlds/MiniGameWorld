9f3a36ad2f5770777870f1c7e563bc9a7b705baa
worldbiomusic <worldbiomusic@gmail.com>
2021-09-30 16:07:59 +0900

    edit detailed event

--- src/com/minigameworld/minigameframes/utils/MiniGameEventDetector.java
+++ src/com/minigameworld/minigameframes/utils/MiniGameEventDetector.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.Event;
@@ -21,6 +22,7 @@ import org.bukkit.event.block.BlockReceiveGameEvent;
 import org.bukkit.event.block.BlockShearEntityEvent;
 import org.bukkit.event.block.CauldronLevelChangeEvent;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.command.UnknownCommandEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityEvent;
@@ -29,11 +31,19 @@ import org.bukkit.event.hanging.HangingEvent;
 import org.bukkit.event.inventory.InventoryEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
+import org.bukkit.event.vehicle.VehicleDestroyEvent;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
+import org.bukkit.event.vehicle.VehicleEvent;
+import org.bukkit.event.vehicle.VehicleExitEvent;
 import org.bukkit.inventory.Inventory;
 
 import com.destroystokyo.paper.event.block.TNTPrimeEvent;
+import com.destroystokyo.paper.event.entity.EnderDragonFireballHitEvent;
+import com.destroystokyo.paper.event.entity.EndermanAttackPlayerEvent;
 
 import io.papermc.paper.event.block.BellRingEvent;
+import io.papermc.paper.event.entity.ElderGuardianAppearanceEvent;
 
 public class MiniGameEventDetector {
 	// detectable events
@@ -59,7 +69,7 @@ public class MiniGameEventDetector {
 
 	// check detectable event
 
-	// [IMPORTANT] never check in detectableEventList (bacause, detectableEventList
+	// [IMPORTANT] never check in detectableEventList (because, detectableEventList
 	// can't not get player in every event)
 	public boolean isDetectableEvent(Event event) {
 		return !this.getPlayersFromEvent(event).isEmpty();
@@ -116,6 +126,10 @@ public class MiniGameEventDetector {
 			this.getPlayersFromBlockEvent((BlockEvent) event, eventPlayers);
 		} else if (event instanceof EntityEvent) {
 			this.getPlayersFromEntityEvent((EntityEvent) event, eventPlayers);
+		} else if (event instanceof VehicleEvent) {
+			this.getPlayersFromVehicleEvent((VehicleEvent) event, eventPlayers);
+		} else if (event instanceof UnknownCommandEvent) {
+			this.getPlayersFromUnknownCommandEvent((UnknownCommandEvent) event, eventPlayers);
 		}
 
 		return !eventPlayers.isEmpty();
@@ -182,8 +196,52 @@ public class MiniGameEventDetector {
 					eventPlayers.add((Player) proj.getShooter());
 				}
 			}
+		} else if (event instanceof ElderGuardianAppearanceEvent) {
+			ElderGuardianAppearanceEvent e = (ElderGuardianAppearanceEvent) event;
+			eventPlayers.add(e.getAffectedPlayer());
+		} else if (event instanceof EnderDragonFireballHitEvent) {
+			EnderDragonFireballHitEvent e = (EnderDragonFireballHitEvent) event;
+			for (LivingEntity entity : e.getTargets()) {
+				if (entity instanceof Player) {
+					eventPlayers.add((Player) entity);
+				}
+			}
+		} else if (event instanceof EndermanAttackPlayerEvent) {
+			eventPlayers.add(((EndermanAttackPlayerEvent) event).getPlayer());
+		}
+
+	}
+
+	private void getPlayersFromVehicleEvent(VehicleEvent event, List<Player> eventPlayers) {
+		if (event instanceof VehicleDamageEvent) {
+			VehicleDamageEvent e = (VehicleDamageEvent) event;
+			if (e.getAttacker() instanceof Player) {
+				eventPlayers.add(((Player) e.getAttacker()));
+			}
+		} else if (event instanceof VehicleDestroyEvent) {
+			VehicleDestroyEvent e = (VehicleDestroyEvent) event;
+			if (e.getAttacker() instanceof Player) {
+				eventPlayers.add(((Player) e.getAttacker()));
+			}
+		} else if (event instanceof VehicleEnterEvent) {
+			VehicleEnterEvent e = (VehicleEnterEvent) event;
+			if (e.getEntered() instanceof Player) {
+				eventPlayers.add(((Player) e.getEntered()));
+			}
+		} else if (event instanceof VehicleExitEvent) {
+			VehicleExitEvent e = (VehicleExitEvent) event;
+			if (e.getExited() instanceof Player) {
+				eventPlayers.add(((Player) e.getExited()));
+			}
 		}
 	}
+
+	private void getPlayersFromUnknownCommandEvent(UnknownCommandEvent event, List<Player> eventPlayers) {
+		if (event.getSender() instanceof Player) {
+			eventPlayers.add((Player) event.getSender());
+		}
+	}
+
 }
 //
 //
