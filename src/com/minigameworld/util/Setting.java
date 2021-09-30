a1f49cac2c44e14801922f0cd4349c08aec65a55
worldbiomusic <worldbiomusic@gmail.com>
2021-09-30 22:41:29 +0900

    add config string

--- src/com/minigameworld/util/Setting.java
+++ src/com/minigameworld/util/Setting.java
@@ -2,15 +2,31 @@ package com.minigameworld.util;
 
 public class Setting {
 	public static final boolean DEBUG_MODE = true;
-	
+
 	public static final String GUI_INV_TITLE = "MiniGameWorld";
-	
+
 	// party
 	public static final int PARTY_INVITE_TIMEOUT = 60;
-	public static final int PARTY_ASK_TIMEOUT= 60;
-	
+	public static final int PARTY_ASK_TIMEOUT = 60;
+
 	// Minigame
 	public static final int MINIGAME_LEAVE_MIN_TIME = 3;
-	
-	
+
+	// settings.yml
+	public static final String SETTINGS_MESSAGE_PREFIX = "message-prefix";
+	public static final String SETTINGS_MINIGAME_COMMAND = "minigame-command";
+	public static final String SETTINGS_MINIGAME_SIGN = "minigame-sign";
+	public static final String SETTINGS_LOBBY = "lobby";
+
+	// minigames
+	public static final String MINIGAMES_TITLE = "title";
+	public static final String MINIGAMES_LOCATION = "location";
+	public static final String MINIGAMES_MIN_PLAYER_COUNT = "min-player-count";
+	public static final String MINIGAMES_MAX_PLAYER_COUNT = "max-player-count";
+	public static final String MINIGAMES_WAITING_TIME = "waiting-time";
+	public static final String MINIGAMES_TIME_LIMIT = "time-limit";
+	public static final String MINIGAMES_ACTIVE = "active";
+	public static final String MINIGAMES_TUTORIAL = "tutorial";
+	public static final String MINIGAMES_CUSTOM_DATA = "custom-data";
+	public static final String MINIGAMES_ICON = "icon";
 }
