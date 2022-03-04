b27b10143f166a534b1cf72d1733b7dc84b2488a
worldbiomusic <worldbiomusic@gmail.com>
2022-03-04 15:13:23 +0900

    Remove Metrics (use Metrics of wbmMC)

--- src/com/worldbiomusic/minigameworld/MiniGameWorldMain.java
+++ src/com/worldbiomusic/minigameworld/MiniGameWorldMain.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.wbm.plugin.util.Metrics;
 import com.worldbiomusic.minigameworld.api.MiniGameWorld;
 import com.worldbiomusic.minigameworld.api.MiniGameWorldUtils;
 import com.worldbiomusic.minigameworld.commands.MiniGameCommand;
@@ -12,7 +13,6 @@ import com.worldbiomusic.minigameworld.listeners.CommonEventListener;
 import com.worldbiomusic.minigameworld.listeners.MiniGameEventListener;
 import com.worldbiomusic.minigameworld.managers.DataManager;
 import com.worldbiomusic.minigameworld.managers.MiniGameManager;
-import com.worldbiomusic.minigameworld.util.Metrics;
 import com.worldbiomusic.minigameworld.util.Setting;
 import com.worldbiomusic.minigameworld.util.UpdateChecker;
 import com.worldbiomusic.minigameworld.util.Utils;
